23687a9854a6632ad3e34f646a0e48247931dad4
yangshujun-YSJ <jiangzhao2025@outlook.com>
2026-05-31 03:05:09 +0800

    主界面、暂停界面、对话界面、设置界面UI布局

--- 时空奇遇记.gd
+++ 时空奇遇记.gd
@@ -6,12 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
 func _on_quit_button_pressed() -> void:
 	get_tree().quit() # Replace with function body.
 
