ffbd683135bc064901168e253c9de67311675be4
yangshujun-YSJ <jiangzhao2025@outlook.com>
2026-05-31 05:44:35 +0800

    主界面设置按钮和开始游戏按钮绑定

--- scripts/时空奇遇记.gd
+++ scripts/时空奇遇记.gd
@@ -1,13 +1,35 @@
 extends Control
 
+const SET_MENU_PATH := "res://scene/set_menu.tscn"
+
+var _set_menu: Control = null
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/level_1.tscn")
+
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
+
 func _on_setting_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Settings.tscn")
+	if _set_menu:
+		return
+
+	var scene := load(SET_MENU_PATH) as PackedScene
+	_set_menu = scene.instantiate() as Control
+	add_child(_set_menu)
+
+	_set_menu.get_node("ExitBtn").pressed.connect(_close_settings)
+	
+
 
 func _on_save_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Save Interface.tscn")
+	pass
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Level1.tscn")
+
+func _close_settings() -> void:
+	if _set_menu:
+		_set_menu.queue_free()
+		_set_menu = null
