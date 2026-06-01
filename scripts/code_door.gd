b9fe75252e03dbe41b531702be4994edfab116cd
oclando707 <15187017680@163.com>
2026-06-01 17:10:37 +0800

    完善0-1关卡A1，部分A2

--- scripts/code_door.gd
+++ scripts/code_door.gd
@@ -7,6 +7,7 @@ var interaction_locked: bool = false
 var unlocked: bool = false
 
 @onready var hint := $FHint if has_node("FHint") else null
+@onready var tab_hint := $TabHint if has_node("TabHint") else null
 
 
 func _ready():
@@ -15,12 +16,18 @@ func _ready():
 
 
 func _process(_delta):
-	if player_in_range and not interaction_locked and Input.is_action_just_pressed("interact"):
+	if player_in_range and not interaction_locked and not unlocked and Input.is_action_just_pressed("interact"):
 		interaction_locked = true
 		if hint:
 			hint.visible = false
 		interacted.emit()
 		_on_unlock()
+
+	# 门解锁后，按 Tab 切换场景
+	if unlocked and player_in_range and Input.is_action_just_pressed("switch"):
+		if tab_hint:
+			tab_hint.visible = false
+		get_tree().change_scene_to_file("res://scene/Level0-2_A2.tscn")
 
 
 func _on_unlock():
@@ -43,12 +50,18 @@ func _on_unlock():
 	# 取消碰撞体，让玩家通过
 	$CollisionShape2D.set_deferred("disabled", true)
 
+	# 显示 Tab 切换提示
+	if tab_hint:
+		tab_hint.visible = true
+
 
 func _on_body_entered(body):
-	if body is CharacterBody2D and not unlocked:
+	if body is CharacterBody2D:
 		player_in_range = true
-		if hint:
+		if not unlocked and hint:
 			hint.visible = true
+		if unlocked and tab_hint:
+			tab_hint.visible = true
 
 
 func _on_body_exited(body):
@@ -56,3 +69,5 @@ func _on_body_exited(body):
 		player_in_range = false
 		if hint:
 			hint.visible = false
+		if tab_hint:
+			tab_hint.visible = false
